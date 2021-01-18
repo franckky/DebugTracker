0247037a747de865759146257f289977c9fe8474
franck <franck_lt@outlook.fr>
2021-01-18 14:04:57 +0100

    DebugTracker.Api
    Create Solution + Projects : Api, Data, Dals, Services

---
+++ DebugTracker.Api/DebugTracker.DALs/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DebugTracker.DALs
+{
+    public class Class1
+    {
+    }
+}
---
+++ DebugTracker.Api/DebugTracker.Services/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DebugTracker.Services
+{
+    public class Class1
+    {
+    }
+}
